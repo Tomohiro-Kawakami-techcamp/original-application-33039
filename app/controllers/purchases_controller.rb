64311bef007cb0c88a513e0f4a920a5ca64a2e94
Tomohiro-Kawakami-techcamp <zhikuanchuanshang725@gmail.com>
2021-01-05 16:31:59 +0900

    並び方変更

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -2,6 +2,6 @@ class PurchasesController < ApplicationController
   before_action :authenticate_user!,  only: [:show]
 
   def show
-    @orders = Order.where(user_id: current_user.id)
+    @orders = Order.where(user_id: current_user.id).order("created_at DESC")
   end
 end
