e6b718b7ddc21b4bf8bbf1ae76210d3bd0428610
Tomohiro-Kawakami-techcamp <zhikuanchuanshang725@gmail.com>
2021-01-09 15:59:50 +0900

    直し

--- app/controllers/service_orders_controller.rb
+++ app/controllers/service_orders_controller.rb
@@ -7,8 +7,8 @@ class ServiceOrdersController < ApplicationController
     @service = Service.find(params[:service_id])
     @service_orders = ServiceOrder.all
     if @service_orders.where(user_id: current_user.id, service_id: params[:service_id]).present?
-       binding.pry
-      render :index
+      flash[:notice] = "既に登録済みです。"
+      redirect_to service_service_orders_path
     else
       @service_order = ServiceOrder.create(user_id: current_user.id, service_id: params[:service_id])
       if @service_order.save
