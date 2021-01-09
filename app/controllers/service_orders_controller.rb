2487d4a6d2cafe283419ace0982f798dae6bdb7c
Tomohiro-Kawakami-techcamp <zhikuanchuanshang725@gmail.com>
2021-01-09 23:50:16 +0900

    少し変更

--- app/controllers/service_orders_controller.rb
+++ app/controllers/service_orders_controller.rb
@@ -6,8 +6,12 @@ class ServiceOrdersController < ApplicationController
   def create
     @service = Service.find(params[:service_id])
     @service_orders = ServiceOrder.all
+    #@user = User.find(params[:user_id])
     if @service_orders.where(user_id: current_user.id, service_id: params[:service_id]).present?
-      flash[:notice] = "既に登録済みです。"
+      flash[:notice] = "既に応募済みです。"
+      redirect_to service_service_orders_path
+    elsif  ((current_user.member_id == 3 || current_user.member_id == 2) && @service.target.include?("年間チケット会員のみ")) || (current_user.member_id == 3  && @service.target.include?("年間チケット、レギュラー会員"))
+      flash[:notice] = "応募できません。"
       redirect_to service_service_orders_path
     else
       @service_order = ServiceOrder.create(user_id: current_user.id, service_id: params[:service_id])
