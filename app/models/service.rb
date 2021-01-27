aac674b75e052c998f644e000c25188eda67f580
Tomohiro-Kawakami-techcamp <zhikuanchuanshang725@gmail.com>
2021-01-27 16:39:37 +0900

    モデル変更等

--- app/models/service.rb
+++ app/models/service.rb
@@ -2,7 +2,7 @@ class Service < ApplicationRecord
   belongs_to :user
 
   validates :content,    presence: true
-  validates :limit_day,  presence: true
+  validates :limit_day,  presence: true, format: { with: /\A\d{4}-\d{2}-\d{2}\z/, message: "input correctly" }
   validates :target,     presence: true
-  validates :release_day,  presence: true
+  validates :release_day,  presence: true, format: { with: /\A\d{4}-\d{2}-\d{2}\z/, message: "input correctly" }
 end
